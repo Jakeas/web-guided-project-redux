037164646deb61f5422d1a259f186290d556e0af
Dustin Myers <dustint314@gmail.com>
2021-04-13 10:53:33 -0600

    wip

--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import Title from './components/Title';
-import DragonList from './components/DragonList';
-import './styles.css';
+import Title from "./components/Title";
+import DragonList from "./components/DragonList";
+import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <Title />
-      <DragonList />
-    </div>
-  );
-}
-
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
