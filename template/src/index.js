a043528514a7f7932c1aa483b7a223350deb4d6d
Dustin Myers <dustint314@gmail.com>
2021-04-13 10:12:25 -0600

    wip

--- template/src/index.js
+++ template/src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import { render } from 'react-dom';
-import './styles.scss';
+import React from "react";
+import { render } from "react-dom";
+import { createStore } from "redux";
+import "./styles.scss";
 
-const App = ()=> {
-    return(<h1>Hello World</h1>);
+// Step 1 - Create a Redux store
+const store = createStore();
+
+const App = () => {
+  return <h1>Hello World</h1>;
 };
 
-render(
-    <App/>,
-    document.querySelector('#root')
-);
+// Step 1a - connect the store to our app
+render(<App />, document.querySelector("#root"));
