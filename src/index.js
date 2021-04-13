f4b0f0f1948b05fafde3bd61959442c3c7ff039e
Dustin Myers <dustint314@gmail.com>
2021-04-13 10:56:39 -0600

    wip

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// STEP 1 - create a store
+
 import App from "./App";
 import "./styles.css";
 
+// STEP 1a - Wrap the app in the Provider component and "provide"
+// the store to your application through props
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
