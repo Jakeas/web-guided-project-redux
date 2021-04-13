a98c39995e16855c90d117676c4f06975f73baf0
Dustin Myers <dustint314@gmail.com>
2021-04-13 11:56:44 -0600

    wip

--- src/reducers/dragonListReducer.js
+++ src/reducers/dragonListReducer.js
@@ -11,7 +11,15 @@ const initialState = {
 export const dragonListReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_MEMBER:
-    // what should we return?
+      // what should we return?
+      const newMember = {
+        name: action.payload,
+        dragonStatus: false,
+      };
+      return {
+        ...state,
+        members: [...state.members, newMember],
+      };
     default:
       return state;
   }
